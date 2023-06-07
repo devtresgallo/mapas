25cdef5ca7b6e9e9844ab095965292dd6f872f68
Daniel Tresgallo <24434932+devtresgallo@users.noreply.github.com>
2023-06-07 14:25:46 +0200

    Add files via upload
    Añadidas areas e iconos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     // Center of the map
-    var center = [38.11568, 13.36146];
+    var center = [38.1157, 13.3615];
     var zoom = 15;
 
     var map = L.map('map').setView(center, zoom);
@@ -20,10 +20,38 @@ $(document).ready(function () {
         opacity: 0.4,
         interactive: true
     }).addTo(map);
-    
+
     /* Elementos añadidos en el mapa */
-    var centerMark = L.marker([38.11568, 13.36146]).addTo(map);
-    centerMark.bindPopup("Centro neurálgico<br>Enclave resistencias");
+    var resistMark = L.marker([38.114, 13.3666]).addTo(map);
+    resistMark.bindPopup("Centro neurálgico<br>Enclave resistencias");
+    
+    var fireArea = L.circle([38.11, 13.353], {
+        color: 'orange',
+        fillColor: 'orange',
+        fillOpacity: 0.4,
+        radius: 400
+    }).addTo(map);
+    fireArea.bindPopup("Revuelta Palermo<br>Incendio provocado");
+
+    var fireIcon = L.icon({
+        iconUrl: 'img/flame-icon.png',
+        iconSize: [46, 61.5],
+        popupAnchor: [-3, -76],
+    });
+
+    var fireMarker = L.marker([38.11, 13.353], {icon: fireIcon}).addTo(map);
+    fireMarker.bindPopup("Revuelta Palermo<br>Incendio provocado");
+
+    var restrictedArea = L.polygon([
+        [38.1157, 13.3615],
+        [38.1182, 13.3677],
+        [38.1233, 13.3647],
+        [38.1213, 13.3579]],{
+            color: 'blue',
+            fillColor: 'red'
+        }
+    ).addTo(map);
+    restrictedArea.bindPopup("<i>Área restringida</i>");
 
     $("#sldOpacity").on('change', function(){
         $("#img-opacity").html(this.value);
